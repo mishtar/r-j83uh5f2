945dce3bb6b58056952f41f144c47948da658a9a
Sebastian Arboleda <SArboleda@arrivelogistics.com>
2018-04-02 19:10:17 -0500

    exercise

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      count : 0,
+      textValue : ''
+    }
+
+    this.contador = this.contador.bind(this);
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.contador} value={this.state.textValue}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
+  }
+
+  contador(event){
+    this.setState({count : event.target.value.length, textValue: event.target.value});
   }
 }
 
